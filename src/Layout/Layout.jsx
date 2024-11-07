8a1088386990b8bb492e938dbd22455022a5458b
Jahid-Hossan <jahidhossen02@gmail.com>
2024-11-07 17:03:28 +0300

    first commit

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "../components/SideBar/SideBar";
+
+const Layout = () => {
+  return (
+    <div className="text-base-color1 font-Poppins flex">
+      <div className="bg-dark-color2 h-[100vh] w-1/4">
+        <SideBar />
+      </div>
+      <div className="bg-dark-color4 h-[100vh] w-full">
+        {/* <Outlet/> */}
+        content
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
